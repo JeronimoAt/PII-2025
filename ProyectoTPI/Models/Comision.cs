b6edc01c4cffafeb3063b7f89299e54c9353848a
1W3-406.195-AhumadaTorres-Jeronimo <160808585+JeronimoAt@users.noreply.github.com>
2025-11-20 21:03:47 -0300

    Add files via upload

---
+++ ProyectoTPI/Models/Comision.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Inmobiliaria.Models
+{
+    [Table("Comisiones")]
+    public partial class Comision
+    {
+        [Key]
+        [Column("Id_Comision")]
+        public int IdComision { get; set; }
+
+        [Column("ID_Empleado")]
+        public int IdEmpleado { get; set; }
+
+        [Column("Fecha")]
+        public DateTime Fecha { get; set; }
+
+        [Column("Importe")]
+        public decimal Importe { get; set; }
+
+        [Column("Observaciones")]
+        public string? Observaciones { get; set; }
+
+        [ForeignKey("IdEmpleado")]
+        public virtual Empleado IdEmpleadoNavigation { get; set; } = null!;
+    }
+}
---
+++ ProyectoTPI/Models/Venta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Inmobiliaria.Models
+{
+    [Table("Ventas")]
+    public partial class Venta
+    {
+        [Key]
+        [Column("Id_Venta")]          // nombre de la columna en SQL
+        public int IdVenta { get; set; }
+
+        [Column("Id_Inmueble")]
+        public int IdInmueble { get; set; }
+
+        [Column("Fecha")]
+        public DateTime Fecha { get; set; }   // o DateOnly si tu contexto usa DateOnly
+
+        [Column("Importe")]
+        public decimal Importe { get; set; }
+
+        [Column("Observaciones")]
+        public string? Observaciones { get; set; }
+
+        // Relación con Inmueble (opcional pero recomendado)
+        [ForeignKey("IdInmueble")]
+        public virtual Inmueble IdInmuebleNavigation { get; set; } = null!;
+    }
+}
